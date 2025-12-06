fa09a1bcd7b2892682445ea9beb987cc40a73d7f
LuanODias <luoliveidias@gmail.com>
2025-12-06 18:17:17 -0300

    feat: ensure EmailAlreadyInUseError is used

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,6 +1,7 @@
 import { CreateUserUseCase } from '../use-cases/create-user.js'
 import validator from 'validator'
 import { badRequest, created, serverError } from './helpers.js'
+import { EmailAlreadyInUserError } from '../errors/user.js'
 
 export class CreateUserController {
     async execute(httpRequest) {
@@ -42,6 +43,9 @@ export class CreateUserController {
 
             return created(createdUser)
         } catch (error) {
+            if (error instanceof EmailAlreadyInUserError) {
+                return badRequest({ message: error.message })
+            }
             console.error(error)
             return serverError()
         }
--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
 import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
 import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { EmailAlreadyInUserError } from '../errors/user.js'
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
@@ -15,7 +16,7 @@ export class CreateUserUseCase {
             )
 
         if (userWithProvidedEmail) {
-            throw new Error('The provided email is already in use.')
+            throw new EmailAlreadyInUserError(createUserParams.email)
         }
 
         // gerar ID do usuário
