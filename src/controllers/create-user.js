e0a51a8a6efda8b4c8bffcb75a234b853b98800e
LuanODias <luoliveidias@gmail.com>
2025-12-06 16:31:17 -0300

    feat: add CreateUserController and validations

---
+++ src/controllers/create-user.js
@@ -0,0 +1,49 @@
+import { CreateUserUseCase } from '../use-cases/create-user.js'
+import validator from 'validator'
+import { badRequest, created, serverError } from './helpers.js'
+
+export class CreateUserController {
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body
+
+            const requiredFields = [
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+            ]
+
+            for (const field of requiredFields) {
+                if (!params[field] || params[field].trim().length === 0) {
+                    return badRequest({ message: `Missing param: ${field}` })
+                }
+            }
+
+            const isValidPassword = params.password.length < 6
+
+            if (isValidPassword) {
+                return badRequest({
+                    message: 'Password must be be at least 6 characters',
+                })
+            }
+
+            const emailIsValid = validator.isEmail(params.email)
+
+            if (!emailIsValid) {
+                return badRequest({
+                    message: 'Invalid E-mail. Please provide a valid one.',
+                })
+            }
+
+            const createUserUseCase = new CreateUserUseCase()
+
+            const createdUser = await createUserUseCase.execute(params)
+
+            return created(createdUser)
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}
