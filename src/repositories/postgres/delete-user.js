8ff3450db4b261f6c559a5342620a8274a44e9f6
LuanODias <luoliveidias@gmail.com>
2025-12-16 15:42:33 -0300

    feat: add DeleteUserUseCase

--- src/repositories/postgres/delete-user.js
+++ src/repositories/postgres/delete-user.js
@@ -1,6 +1,6 @@
 import { PostgresHelper } from '../../db/postgres/helper.js'
 
-export class postgresDeleteUser {
+export class PostgresDeleteUserRepository {
     async execute(userId) {
         const deletedUser = await PostgresHelper.query(
             `
