b7261a92cc1f4742452c5816215bdc0a10602ba9
LuanODias <luoliveidias@gmail.com>
2026-01-13 16:29:55 -0300

    fix: fix CreateTransactionController required fields validation

--- src/controllers/transaction/create-transaction.js
+++ src/controllers/transaction/create-transaction.js
@@ -4,10 +4,10 @@ import {
     serverError,
     checkIfIdIsValid,
     invalidIdResponse,
-} from '../helpers.js'
-import { created } from '../helpers'
+    created,
+} from '../helpers/index.js'
 
-export class createTransactionController {
+export class CreateTransactionController {
     constructor(createTransactionUseCase) {
         this.createTransactionUseCase = createTransactionUseCase
     }
@@ -15,17 +15,13 @@ export class createTransactionController {
         try {
             const params = httpRequest.body
 
-            const requiredFields = [
-                'id',
-                'user_id',
-                'name',
-                'date',
-                'amount',
-                'type',
-            ]
+            const requiredFields = ['user_id', 'name', 'date', 'amount', 'type']
 
             for (const field of requiredFields) {
-                if (!params[field] || params[field].trim().length === 0) {
+                if (
+                    !params[field] ||
+                    params[field].toString().trim().length === 0
+                ) {
                     return badRequest({ message: `Missing param: ${field}` })
                 }
             }
