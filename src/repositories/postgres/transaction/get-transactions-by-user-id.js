b0abc56cfbac3c84285e56a8a3bf67d4ffcdf488
LuanODias <luoliveidias@gmail.com>
2026-01-15 23:35:29 -0300

    feat: ensure UpdateTransactionRepository and GetTransactionByUserId use prisma

--- src/repositories/postgres/transaction/get-transactions-by-user-id.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -1,11 +1,11 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetTransactionsByUserIdRepository {
     async execute(user_id) {
-        const transactions = await PostgresHelper.query(
-            'SELECT * FROM transactions WHERE user_id = $1',
-            [user_id],
-        )
-        return transactions
+        return await prisma.transaction.findMany({
+            where: {
+                user_id,
+            },
+        })
     }
 }
