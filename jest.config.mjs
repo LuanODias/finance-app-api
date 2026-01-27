0839bc1d7d820f1b9fa66a6ae1e72604be6095c6
LuanODias <luoliveidias@gmail.com>
2026-01-27 16:07:22 -0300

    chore: fix jest config adding ignore paths

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('jest').Config} */
 const config = {
-  
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    watchPathIgnorePatterns: [
+        '<rootDir>/node_modules/',
+        '<rootDir>/.postgres-data/',
+    ],
+    modulePathIgnorePatterns: [
+        '<rootDir>/node_modules/',
+        '<rootDir>/.postgres-data/',
+    ],
+}
 
-};
-
-export default config;
+export default config
