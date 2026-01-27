6a5080adf7efb1f1c50d24558df7c0289dac3fc8
LuanODias <luoliveidias@gmail.com>
2026-01-27 16:08:57 -0300

    test: ensure 400 is returned if last_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -35,8 +35,27 @@ describe('CreateUserController', () => {
 
         const httpRequest = {
             body: {
-                first_name: '',
                 last_name: 'Doe',
+                email: 'john.doe@example.com',
+                password: 'password',
+            },
+        }
+
+        //act
+        const result = await createUserController.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 if last_name is not provided', async () => {
+        //arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'John',
                 email: 'john.doe@example.com',
                 password: 'password',
             },
